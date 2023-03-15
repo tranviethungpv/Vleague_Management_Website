27e4560e115fff0b8d20da800d80587d84cc1b4a
HoangLong <84828423+ninhhoanglong@users.noreply.github.com>
2023-03-16 00:43:24 +0700

    up lichThiDau

---
+++ Vleague_Management_Website/Controllers/LichThiDauController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vleague_Management_Website.Models;
+
+namespace Vleague_Management_Website.Controllers
+{
+    public class LichThiDauController : Controller
+    {
+        QlbongDaContext db = new QlbongDaContext();
+        public IActionResult Index()
+        {
+            var listMatch = (from a in db.Trandaus 
+                             join b in db.Caulacbos on a.Clbnha equals b.CauLacBoId
+                             join c in db.Caulacbos on a.Clbkhach equals c.CauLacBoId
+                             select new LichThiDau
+                             {
+                                 TenClbNha = b.TenClb,
+                                 TenClbKhach = c.TenClb,
+                                 StartTime = a.NgayThiDau
+                             }).Take(10).ToList();
+            return View(listMatch);
+        }
+    }
+}
+
+public class LichThiDau
+{
+    public String TenClbNha;
+    public String TenClbKhach;
+    public DateTime? StartTime;
+}
--- Vleague_Management_Website/Models/QlbongDaContext.cs
+++ Vleague_Management_Website/Models/QlbongDaContext.cs
@@ -31,7 +31,7 @@ public partial class QlbongDaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING"));
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-VLFN3LR;Initial Catalog=QLBongDa;Integrated Security=True; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
