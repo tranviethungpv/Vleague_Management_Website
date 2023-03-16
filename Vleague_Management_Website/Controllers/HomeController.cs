48d7f718d4b380666c5310db20989a43ece30201
TuTran2408 <tu201210384@lms.utc.edu.vn>
2023-03-16 19:02:33 +0700

    check mic

--- Vleague_Management_Website/Controllers/HomeController.cs
+++ Vleague_Management_Website/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace Vleague_Management_Website.Controllers
 {
     public class HomeController : Controller
     {
+       QlbongDaContext db = new QlbongDaContext();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,9 +16,54 @@ namespace Vleague_Management_Website.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+            List<Caulacbo> lstCauLacBo = db.Caulacbos.ToList();
+            List<global::LeaderBoard> lstBangXepHang = new List<global::LeaderBoard>();
+            foreach (var item in lstCauLacBo)
+            {
+                List<Trandau> lstTranDau = db.Trandaus.Where(x => x.Clbnha == item.CauLacBoId).ToList();
+                var soTranThang = 0;
+                var soTranThua = 0;
+                var soTranHoa = 0;
+                var diem = 0;
 
+                foreach (var item1 in lstTranDau)
+                {
+                    string[] ketqua = item1.KetQua.Split("-");
+                    var banThangDoiNha = int.Parse(ketqua[0]);
+                    var banThangDoiKhach = int.Parse(ketqua[1]);
+                    if (banThangDoiNha > banThangDoiKhach)
+                    {
+                        soTranThang += 1;
+                    }
+                    if (banThangDoiNha < banThangDoiKhach)
+                    {
+                        soTranThua += 1;
+                    }
+                    if (banThangDoiNha == banThangDoiKhach)
+                    {
+                        soTranHoa += 1;
+                    }
+                }
+                diem = soTranThang * 3 + soTranHoa;
+                lstBangXepHang.Add(new global::LeaderBoard
+                {
+                    TenClb = item.TenClb,
+                    SoTranThang = soTranThang,
+                    SoTranHoa = soTranHoa,
+                    SoTranThua = soTranThua,
+                    Diem = diem,
+                });
+
+            }
+            var lstBangXepHangSort = lstBangXepHang.OrderByDescending(x => x.Diem).ToList();
+            return View(lstBangXepHangSort);
+        
+        }
+        public IActionResult ChiTietCauThu(string macauthu)
+        {
+            var cauthu = db.Cauthus.SingleOrDefault(x=>x.CauThuId==macauthu);
+            return View(cauthu);
+        }
         public IActionResult Privacy()
         {
             return View();
@@ -30,3 +76,13 @@ namespace Vleague_Management_Website.Controllers
         }
     }
 }
+public class LeaderBoard
+{
+    public String TenClb;
+    public int SoTranThang;
+    public int SoTranThua;
+    public int SoTranHoa;
+    public int Diem;
+
+}
+
