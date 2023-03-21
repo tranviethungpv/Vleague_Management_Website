c79dff693b2ea1ca0e7a02301fb2a7b11f5fa790
LCKy89 <canhky0212@gmail.com>
2023-03-21 08:51:56 +0700

    Ky: Profiles and Clubs

---
+++ Vleague_Management_Website/Controllers/ClubsController.cs
@@ -0,0 +1,28 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using Vleague_Management_Website.Models;
+using X.PagedList;
+
+namespace Vleague_Management_Website.Controllers
+{
+	public class ClubsController : Controller
+	{
+		QlbongDaContext db = new QlbongDaContext();
+		public IActionResult Index(int ? page)
+		{
+			int pageSize = 1;
+			int pageNumber = page == null || page < 0 ? 1 : page.Value;
+			var listClubs = db.Caulacbos.OrderBy(x => x.CauLacBoId).ToList();
+			PagedList<Caulacbo> lst = new PagedList<Caulacbo>(listClubs, pageNumber, pageSize);
+			//var imageProduct = db.TAnhSps.Where(x => x.MaSp == masanpham).ToList();
+			//ViewBag.imageProduct = imageProduct;
+			//if (product == null)
+			//{
+			//	return NotFound();
+			//}
+			return View(lst);
+		}
+	}
+}
---
+++ Vleague_Management_Website/Controllers/ProfilesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vleague_Management_Website.Models;
+using X.PagedList;
+
+namespace Vleague_Management_Website.Controllers
+{
+	public class ProfilesController : Controller
+	{
+		QlbongDaContext db = new QlbongDaContext();
+		public IActionResult Index(int ? page)
+		{
+			int pageSize = 8;
+			int pageNumber = page == null || page < 0 ? 1 : page.Value;
+			var listCT = db.Cauthus.OrderBy(x => x.CauThuId).ToList();
+			PagedList<Cauthu> lst = new PagedList<Cauthu>(listCT, pageNumber, pageSize);
+			return View(lst);
+		}
+		public IActionResult ThongTinCauThu(string CauThu)
+		{
+			var product = db.Cauthus.SingleOrDefault(x => x.CauThuId == CauThu);
+			//var imageProduct = db.Cauthus.Where(x => x.CauThuId == CauThu).ToList();
+			//ViewBag.imageProduct = imageProduct;
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+
+	}
+}
