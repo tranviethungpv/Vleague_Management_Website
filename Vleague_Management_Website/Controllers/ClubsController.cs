99b12e29043d67ecc9db24889da0cb136ec05b61
tvhungpv <tranviethungpv@gmail.com>
2023-03-24 11:21:09 +0700

    tvhung added ChiTietClub

--- Vleague_Management_Website/Controllers/ClubsController.cs
+++ Vleague_Management_Website/Controllers/ClubsController.cs
@@ -10,19 +10,35 @@ namespace Vleague_Management_Website.Controllers
 	public class ClubsController : Controller
 	{
 		QlbongDaContext db = new QlbongDaContext();
-		public IActionResult Index(int ? page)
+		public IActionResult Index(int? page)
 		{
-			int pageSize = 1;
+			int pageSize = 8;
 			int pageNumber = page == null || page < 0 ? 1 : page.Value;
-			var listClubs = db.Caulacbos.OrderBy(x => x.CauLacBoId).ToList();
-			PagedList<Caulacbo> lst = new PagedList<Caulacbo>(listClubs, pageNumber, pageSize);
-			//var imageProduct = db.TAnhSps.Where(x => x.MaSp == masanpham).ToList();
-			//ViewBag.imageProduct = imageProduct;
-			//if (product == null)
-			//{
-			//	return NotFound();
-			//}
+			var listClb = db.Caulacbos.OrderBy(x => x.CauLacBoId).ToList();
+			PagedList<Caulacbo> lst = new PagedList<Caulacbo>(listClb, pageNumber, pageSize);
 			return View(lst);
+		}
+		public IActionResult ChiTietClub(string clubid)
+		{
+			var listClub = (from a in db.Caulacbos
+							 join b in db.Sanvandongs on a.SanVanDongId equals b.SanVanDongId
+							 join c in db.Huanluyenviens on a.HuanLuyenVienId equals c.HuanLuyenVienId
+							 where a.CauLacBoId == clubid
+							 select new ChiTietCauLacBo
+							 {
+								 TenClb = a.TenClb,
+								 TenGoi = a.TenGoi,
+								 Sanvandong = b.TenSan,
+								 Huanluyenvien = c.TenHlv
+							 }).ToList();
+			return View(listClub);
+		}
+		public class ChiTietCauLacBo
+		{
+			public String? TenClb;
+			public String? TenGoi;
+			public String? Sanvandong;
+			public String? Huanluyenvien;
 		}
 	}
 }
