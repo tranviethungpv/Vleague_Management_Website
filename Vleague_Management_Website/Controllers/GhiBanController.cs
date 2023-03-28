14ec95d6d92f37350ec7f8d887d62f0b710f7c4a
tvhungpv <tranviethungpv@gmail.com>
2023-03-28 09:09:36 +0700

    CSS News, count goals are fixed

--- Vleague_Management_Website/Controllers/GhiBanController.cs
+++ Vleague_Management_Website/Controllers/GhiBanController.cs
@@ -12,12 +12,14 @@ namespace Vleague_Management_Website.Controllers
 
             var result = (from a in db.Cauthus
                           join b in db.TrandauGhibans on a.CauThuId equals b.CauThuId
+                          join c in db.Caulacbos on a.CauLacBoId equals c.CauLacBoId
                           group b by b.CauThuId into g
                           select new
                           {
                               cauthuid = g.Key,
                               tencauthu = g.FirstOrDefault().CauThu.HoVaTen,
-                              sobanthang = g.Sum(x => x.ThoiDiemGhiBan),
+                              sobanthang = g.Count(),
+                              caulacbo = g.FirstOrDefault().CauLacBo.TenClb
                           }).ToList().OrderByDescending((x => x.sobanthang));
             Console.WriteLine(result);
             return View(result);
--- Vleague_Management_Website/Models/QlbongDaContext.cs
+++ Vleague_Management_Website/Models/QlbongDaContext.cs
@@ -37,7 +37,7 @@ public partial class QlbongDaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-U4LQRN2\\SQLEXPRESS;Initial Catalog=QLBongDa;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        => optionsBuilder.UseSqlServer("Data Source=TVHUNG-LATITUDE\\SQLEXPRESS;Initial Catalog=QLBongDa;Integrated Security=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
