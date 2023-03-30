8adde35b1b81e22568037851aedb75582d9a9d57
tvhungpv <tranviethungpv@gmail.com>
2023-03-30 16:28:53 +0700

    Added Area Admin

---
+++ Vleague_Management_Website/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vleague_Management_Website.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
