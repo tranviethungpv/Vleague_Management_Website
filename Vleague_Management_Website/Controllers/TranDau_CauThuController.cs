16aac64a048de3ff3f08ae5c4a528ac264459e24
NHBut <bmontageh@gmail.com>
2023-04-07 22:03:46 +0700

    user , trandaucauthu

---
+++ Vleague_Management_Website/Controllers/TranDau_CauThuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vleague_Management_Website.Controllers
+{
+    public class TranDau_CauThuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
