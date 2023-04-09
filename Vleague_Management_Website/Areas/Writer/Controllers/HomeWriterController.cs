e71775c03c4ec0daa3e81c9f7db5f7d12bbdde17
tvhungpv <tranviethungpv@gmail.com>
2023-04-09 09:34:49 +0700

    [done] News Management (Admin page, Writer)

---
+++ Vleague_Management_Website/Areas/Writer/Controllers/HomeWriterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vleague_Management_Website.Models;
+
+namespace Vleague_Management_Website.Areas.Writer.Controllers
+{
+    [Area("writer")]
+    [Route("writer")]
+    [Route("writer/homewriter")]
+    public class HomeWriterController : Controller
+    {
+        QlbongDaContext db = new QlbongDaContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("TinTuc")]
+        public IActionResult TinTuc()
+        {
+            var listTinTuc = db.TinTucs.Select(x => x).ToList();
+            return View(listTinTuc);
+        }
+    }
+}
